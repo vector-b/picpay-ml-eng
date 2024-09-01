f8b40f83a84be3e20002fa5fef5ad53e8ec8399f
vector-b <vgabriel2011@gmail.com>
2024-09-01 12:26:45 -0300

    Implementing tests

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -1,26 +1,40 @@
-from fastapi.testclient import TestClient
-from main import app
+from apis.app import app
+import pytest
+import json
 
-client = TestClient(app)
 
-def test_health():
+@pytest.fixture
+def client():
+    app.testing = True
+    with app.test_client() as client:
+        yield client
+
+def test_health(client):
     response = client.get("/health")
     assert response.status_code == 200
-    assert response.json() == {"status": "ok"}
+    json_data = response.get_json()
+    assert json_data["status"] == "ok"
 
-
-#test load, predict, history
 def test_load():
     response = client.post("/model/load")
     assert response.status_code == 200
     assert response.json() == {"model": "model"}
 
-def test_predict():
-    response = client.post("/model/predict")
+def test_predict(client):
+    sample_data = {
+        "feature1": 3.5,
+        "feature2": 42,
+        "feature3": 1.8,
+    }
+    
+    response = client.post('/model/predict', json=sample_data)
+    
     assert response.status_code == 200
-    assert response.json() == {"prediction": "prediction"}
+    
+    data = json.loads(response.data)
+    assert "prediction" in data
+    assert isinstance(data["prediction"], float) 
 
-def test_history():
+def test_history(client):
     response = client.get("/model/history")
     assert response.status_code == 200
-    assert response.json() == {"history": "history"}
