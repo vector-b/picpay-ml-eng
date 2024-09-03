7cb10729c8c2b96eedcd4ebbc18f862f8c31ca70
vector-b <vgabriel2011@gmail.com>
2024-09-02 22:23:55 -0300

    fix security issues

--- apis/weatherbit.py
+++ apis/weatherbit.py
@@ -1,10 +1,8 @@
-"1f22b701b6904b66a169a5f52eddbed4"
-
 import requests
 import json
 
 class WeatherBitDB:
-    ApiToken = '1f22b701b6904b66a169a5f52eddbed4'
+    ApiToken = ''
 
     @classmethod
     def get_weather(cls, lat, lon, start_date, end_date):
@@ -20,17 +18,19 @@ class WeatherBitDB:
         'Accept': 'application/json',
         }
 
+
         response = requests.get(url, headers=headers, params=params)
+
+        #print(f"Request for {lat}, {lon} from {start_date} to {end_date}: Status Code - {response.status_code}")
         if response.status_code == 200:
             return response.json()
         else:
             return None
 
     @classmethod
-    def get_weather_list(cls, lat_lon_list, start_date, end_date):
+    def get_weather_list(cls, lat_lon_dates):
         weather_data = {}
-        for lat, lon in lat_lon_list:
-            data = cls.get_weather(lat, lon, start_date, end_date)
-            if data:
-                weather_data[(lat, lon)] = data
+        for lat_lon_date in lat_lon_dates:
+            lat, lon, start_date, end_date = lat_lon_date
+            weather_data[(lat, lon, start_date, end_date)] = cls.get_weather(lat, lon, start_date, end_date)['data'][0]['wind_spd']
         return weather_data
--- test_api.py
+++ test_api.py
@@ -2,4 +2,12 @@ test_list = ['LGA', 'EWR', 'JFK', 'PSE', 'MSY']
 
 from apis.airportdb import AirportDB
 
-AirportDB.get_airport_data(test_list)
+#AirportDB.get_airport_data(test_list)
+
+from apis.weatherbit import WeatherBitDB
+
+test_list_2 = [(40.692501068115234, -74.168701171875, '2013-01-01', '2013-01-02'),
+ (40.777198791503906, -73.87259674072266, '2013-01-01', '2013-01-02')]
+
+list  = WeatherBitDB.get_weather_list(test_list_2)
+print(list)
